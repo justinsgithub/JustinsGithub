f936f8c5dbb296fb9b6fecf50ecb9fd8014dc310
justinsgithub <justinaawd@gmail.com>
2021-12-02 15:18:25 -0800

    added Rubies

---
+++ Rubies/blog/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "articles#index"
+
+    #  get "/articles", to: "articles#index"
+    #  get "/articles/:id", to: "articles#show"
+    resources :articles do
+    resources :comments
+  end
+end
