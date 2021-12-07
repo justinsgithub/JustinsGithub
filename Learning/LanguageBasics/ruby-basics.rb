fa49c4baa1f8e6cdef5666f9bec49202b17a0cc4
Justin Angeles <justinaawd@gmail.com>
2021-12-07 01:52:24 -0800

    Update ruby-basics.rb

--- Learning/LanguageBasics/ruby-basics.rb
+++ Learning/LanguageBasics/ruby-basics.rb
@@ -22,7 +22,6 @@ multi-line
 comment
 =end
 
-=begin
 sum  = 13 + 379
 product = 923 * 15
 quotient = 13209 / 17
@@ -105,7 +104,7 @@ puts is_false
 puts is_true
 
 
-=end
+
 #test_1 = 17 > 16
 
 #test_2 = 21 < 30
@@ -116,23 +115,70 @@ puts is_true
 
 
 # test_1 = 77 != 77
-#test_1 = false
+test_1 = false
 
 # test_2 = -4 <= -4
-#test_2 = true
+test_2 = true
 
 # test_3 = -44 < -33
-#test_3 = true
+test_3 = true
 
 # test_4 = 100 == 1000
-#test_4 = false
+test_4 = false
 
 
 # boolean_1 = 77 < 78 && 77 < 77
-#boolean_1 = false
+boolean_1 = false
 
 # boolean_2 = true && 100 >= 100
-#boolean_2 = true
+boolean_2 = true
 
 # boolean_3 = 2**3 == 8 && 3**2 == 9
-#boolean_3 = true
+boolean_3 = true
+
+# boolean_1 = 2**3 != 3**2 || true
+boolean_1 = true
+
+# boolean_2 = false || -10 > -9
+boolean_2 = false
+
+# boolean_3 = false || false
+boolean_3 = false
+
+# boolean_1 = !true
+boolean_1 = false
+
+# boolean_2 = !true && !true
+boolean_2 = false
+
+# boolean_3 = !(700 / 10 == 70)
+boolean_3 = false
+
+# boolean_1 = (3 < 4 || false) && (false || true)
+boolean_1 = true
+
+# boolean_2 = !true && (!true || 100 != 5**2)
+boolean_2 = false
+
+# boolean_3 = true || !(true || false)
+boolean_3 = true
+
+if 6 < 5 || 6 == 5
+  puts "6 is less than 5"
+elsif 6 > 7 || 687 == "six ate 7"
+  puts "6 is greater than 7"
+else
+  puts "idk wtf is going on"
+end
+
+my_bday = "october"
+
+this_month = "december"
+
+unless this_month == my_bday
+  puts "not my bday month"
+end
+
+problem = false
+
+print "Good to go!" unless problem
