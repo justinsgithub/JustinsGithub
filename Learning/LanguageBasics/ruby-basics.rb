8336e379fbebf1f6a5361bf477437e0721baa5be
justinsgithub <justinaawd@gmail.com>
2021-12-06 22:30:50 -0800

    taking down cheatsheet site, does not serve me

--- Learning/LanguageBasics/ruby-basics.rb
+++ Learning/LanguageBasics/ruby-basics.rb
@@ -1,29 +1,18 @@
 # Ruby Comment
 
 my_num =  25  
-
 my_boolean = true 
-
 my_string = "Ruby"
-
 puts my_num
 puts my_boolean
 puts my_string
-
-
 puts "What's up?"
 puts "Oxford Montalvo" # new line
-
-
 print "What's up?"
 print "Oxford Montalvo" # same line
-
 puts "puts 6".length
-
 puts "erehT olleH".reverse
-
 puts "Justin".upcase 
-
 puts "Justin".downcase 
 
 =begin
@@ -33,6 +22,7 @@ multi-line
 comment
 =end
 
+=begin
 sum  = 13 + 379
 product = 923 * 15
 quotient = 13209 / 17
@@ -83,3 +73,66 @@ print("What's the abbreviation of the state you are from?")
 state = gets.chomp
 state.upcase!
 puts "You are from #{state}!"
+# control flow
+
+print "Integer please: "
+user_num = Integer(gets.chomp)
+puts user_num
+
+puts "type Y to continue, H for help"
+answer = gets.chomp
+if answer.upcase == "Y"
+  puts "you chose yes"
+elsif answer.upcase == "H"
+  puts "sorry no help here"
+else 
+  puts "qutting now"
+end
+
+hungry = false
+
+unless hungry
+  puts "I'm writing Ruby programs!"
+else
+  puts "Time to eat!"
+end
+
+is_true = 2 != 3
+
+is_false = 2 == 3
+
+puts is_false
+puts is_true
+
+
+=end
+#test_1 = 17 > 16
+
+#test_2 = 21 < 30
+
+#test_3 = 9 <= 9
+
+#test_4 = -11 < 4
+
+
+# test_1 = 77 != 77
+#test_1 = false
+
+# test_2 = -4 <= -4
+#test_2 = true
+
+# test_3 = -44 < -33
+#test_3 = true
+
+# test_4 = 100 == 1000
+#test_4 = false
+
+
+# boolean_1 = 77 < 78 && 77 < 77
+#boolean_1 = false
+
+# boolean_2 = true && 100 >= 100
+#boolean_2 = true
+
+# boolean_3 = 2**3 == 8 && 3**2 == 9
+#boolean_3 = true
