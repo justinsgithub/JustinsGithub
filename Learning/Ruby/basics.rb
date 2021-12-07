4292c5d9e5bc8f4b76632ca5a917c7b2c5a747f4
Justin Angeles <justinaawd@gmail.com>
2021-12-06 21:23:33 -0800

    Update and rename script.rb to basics.rb

---
+++ Learning/Ruby/basics.rb
@@ -0,0 +1,27 @@
+# Ruby Comment
+
+my_num =  25  
+
+my_boolean = true 
+
+my_string = "Ruby"
+
+puts my_num
+puts my_boolean
+puts my_string
+
+
+puts "What's up?"
+puts "Oxford Montalvo" # new line
+
+
+print "What's up?"
+print "Oxford Montalvo" # same line
+
+puts "puts 6".length
+
+puts "erehT olleH".reverse
+
+puts "Justin".upcase 
+
+puts "Justin".downcase 
