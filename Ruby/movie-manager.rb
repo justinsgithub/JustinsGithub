4b4b2a0ab2a9a4ed1138936963596b4e4f79c0ed
Justin Angeles <justinaawd@gmail.com>
2021-12-07 19:57:45 -0800

    Create movie-manager.rb

---
+++ Ruby/movie-manager.rb
@@ -0,0 +1,44 @@
+
+movies = {superman: 5}
+
+puts "enter your choice"
+choice = gets.chomp
+
+case choice
+  when "add"
+    puts "enter movie title to add"
+    title = gets.chomp
+    if movies[title.to_sym]
+      puts "movie already exists"
+    else
+      puts "enter movie rating"
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+      puts "movie saved"
+    end
+  when "update"
+    puts "enter movie title to update"
+    title = gets.chomp
+    if !(movies[title.to_sym])
+      puts "movie does not exist"
+    else
+      puts "enter movie rating"
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+      puts "movie updated"
+    end
+  when "display"
+    movies.each {|movie, rating|
+      puts "#{movie}: #{rating}"
+    }
+  when "delete"
+    puts "enter movie title to delete"
+    title = gets.chomp
+    if !(movies[title.to_sym])
+      puts "movie does not exist, nothing to delete"
+    else
+      movies.delete(title.to_sym)
+    end
+else 
+    puts "Error!"
+end
