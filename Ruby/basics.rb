65d0b9a7830c00b2550c2d6368125e1d8644f907
Justin Angeles <justinaawd@gmail.com>
2021-12-07 21:49:45 -0800

    Update basics.rb

--- Ruby/basics.rb
+++ Ruby/basics.rb
@@ -1,5 +1,63 @@
 # ELEGANT RUBY
 
+
+
+
+
+def square(num)
+  puts "implicit return"
+  num ** 2
+end
+
+favorite_animal ||= "cat"
+
+puts "What's your favorite language?"
+
+language = gets.chomp
+
+
+case language
+  when "Ruby" then puts "Ruby is great for web apps!"
+  when "Python" then puts "Python is great for science."
+  when "JavaScript" then puts "JavaScript makes websites awesome."
+  when "HTML" then puts "HTML is what websites are made of!"
+  when "CSS" then puts "CSS makes websites pretty."
+  else puts "I don't know that language!"
+end
+    
+puts 1 < 2 ? "One is less than two!" : "One is not less than two."
+
+
+puts "One is less than two!" if 1 < 2
+
+favorite_things = ["Ruby", "espresso", "candy"]
+
+puts "A few of my favorite things:"
+
+favorite_things.each do |thing|
+  puts "I love #{thing}!"
+end
+
+alphabet = ["a", "b", "c"]
+alphabet << "d" 
+caption = "one, two, three, and "
+caption << "four!"
+
+
+
+age = 26
+
+
+age.respond_to?(:next)
+
+
+"L".upto("P") {|letter| puts letter} 
+
+
+my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+my_array.each {|num| puts num if num % 2 == 0 }
+
 def a
   puts "A was evaluated!"
   return true
