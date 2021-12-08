0622db4ce05ea5aa84a54ef3787ee68bb1897670
Justin Angeles <justinaawd@gmail.com>
2021-12-08 07:57:21 -0800

    Create code_challenges.py

---
+++ Python/code_challenges.py
@@ -0,0 +1,17 @@
+def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
+    if budget < (food_bill + electricity_bill + internet_bill + rent)
+        return True
+    return False
+
+def large_power(base, exponent):
+  if base ** exponent > 5000:
+    return True
+  else:
+    return False
+
+def twice_as_large(num1, num2):
+    if num1 > (num2 * 2):
+        return True
+    return False
+    
+def divisible_by_ten(
