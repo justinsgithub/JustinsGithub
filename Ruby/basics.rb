9520da38ff04fc8586743709ae9ac6ad99837c9c
Justin Angeles <justinaawd@gmail.com>
2021-12-07 17:24:08 -0800

    Update basics.rb

--- Ruby/basics.rb
+++ Ruby/basics.rb
@@ -1,5 +1,21 @@
 # HASHES AND SYMBOLS
 
+movie_ratings = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 3,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+
+puts movie_ratings.each_key {|key| puts key}
+
+good_movies = movie_ratings.select{|movie, rating| rating > 3
+}
 movies = {
   superman:"movie about superman",
   batman:"movie about batman",
