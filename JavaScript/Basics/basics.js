c6400109491ed04fa4ccd3e20941d132e007d5da
Justin Angeles <justinaawd@gmail.com>
2021-12-08 11:06:08 -0800

    Update basics.js

--- JavaScript/Basics/basics.js
+++ JavaScript/Basics/basics.js
@@ -1,1 +1,60 @@
+console.log(Math.random() * 100);
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar ++;
+lostDollar -- ;
+
+const favoriteAnimal = "armadillo";
+
+console.log("My favorite animal: " + favoriteAnimal);
+
+const myName = "Justin";
+
+const myCity = "Kenai";
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`;
+
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof newVariable);
+
+newVariable = 1;
+console.log(typeof newVariable);
+
+// weather converter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
