9902d07632c04ecca54ddf581a7f506f9dcb75fd
Justin Angeles <justinaawd@gmail.com>
2021-12-08 17:27:39 -0800

    Update basics.js

--- JavaScript/Basics/basics.js
+++ JavaScript/Basics/basics.js
@@ -1,3 +1,53 @@
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  if  (region === "The Arctic"){
+    let lightWaves = "Northern Lights";
+    console.log(lightWaves);
+  }
+  console.log(lightWaves);
+};
+
+// the if statement does not change the first lightWaves assignment
+logVisibleLightWaves();
+
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+  stars = "Sirius";
+	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(callMyNightSky());
+console.log(stars)
+
+
+/* throws error 
+const logVisibleLightWaves = () => {
+  const lightWaves = "Moonlight";
+  console.log(lightWaves)
+}
+
+console.log(logVisibleLightWaves())
+
+console.log(lightWaves)
+*/
+
+const satellite = "The Moon";
+
+const galaxy = "The Milky Way";
+
+const stars = "North Star";
+
+const callMyNightSky = () => {
+  return "Night Sky: " + satellite + ", " + stars + ", and " + galaxy;
+}
+
+console.log(callMyNightSky())
+
 const city = "New York City"
 
 const logCitySkyline = () => {
