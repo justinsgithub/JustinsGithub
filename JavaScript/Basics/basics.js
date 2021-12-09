9b0d9d9914ca95e6dc1718b42ddb54bf5d4efee0
Justin Angeles <justinaawd@gmail.com>
2021-12-08 18:13:33 -0800

    Update basics.js

--- JavaScript/Basics/basics.js
+++ JavaScript/Basics/basics.js
@@ -1,7 +1,84 @@
+
+
+
+const numberClusters = [[1,2], [3,4],[5,6]]
+
+const target = numberClusters[2][1]
+
+const numberClusters = [[1,2], [3,4],[5,6]]
+
+const target = numberClusters[2][1]
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift()
+console.log(groceryList)
+
+groceryList.unshift("popcorn")
+
+console.log(groceryList)
+
+console.log(groceryList.slice(1,4))
+console.log(groceryList)
+
+const pastaIndex = groceryList.indexOf("pasta")
+console.log(pastaIndex)
+
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop()
+
+console.log(chores)
+
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push("munch", "snack")
+
+console.log(chores)
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = "Mayo";
+
+console.log(condiments);
+
+condiments = ["Mayo"];
+
+console.log(condiments);
+
+utensils[3] = "Spoon";
+
+console.log(utensils);
+console.log(utensils.length);
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0]
+const firstItem = famousSayings[0]
+
+console.log(listItem)
+console.log(famousSayings[2])
+console.log(famousSayings[3]) // undefined
+
+
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+listItem = famousSayings[0]
+
+console.log(listItem)
+
+
+const hobbies = ["eat", "munch", "snack"]
+
+console.log(hobbies)
+
 const logVisibleLightWaves = () => {
   let lightWaves = 'Moonlight';
 	let region = 'The Arctic';
-  // Add if statement here:
   if  (region === "The Arctic"){
     let lightWaves = "Northern Lights";
     console.log(lightWaves);
@@ -181,4 +258,3 @@ console.log(typeof newVariable);
 
 newVariable = 1;
 console.log(typeof newVariable);
-
