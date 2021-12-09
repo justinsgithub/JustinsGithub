c740c39e8b6ce200e037293d3ffde7e7000465c4
Justin Angeles <justinaawd@gmail.com>
2021-12-09 08:52:52 -0800

    Update object-orientated.rb

--- Ruby/Basics/object-orientated.rb
+++ Ruby/Basics/object-orientated.rb
@@ -1,4 +1,40 @@
 
+class Account
+  attr_reader :name
+  attr_reader :balance
+  def initialize(name, balance=100)
+    @name=name
+    @balance=balance
+  end
+
+  public
+
+  def display_balance(pin_number)
+    puts pin_number == pin ? "Balance: $#{@balance}" : pin_error
+  end
+
+  def withdraw(pin_number, amount)
+    if pin_number == pin
+      @balance -= amount
+      puts "Withdrew #{amount}. New balance: $#{@balance}"  
+    else
+      puts pin_erorr
+    end
+  end
+
+  private
+  
+  def pin
+    @pin = 1234
+  end
+  
+  def pin_error
+    return "Access denied: incorrect PIN."
+  end
+
+end
+
+checking_account = Account.new("Justin", 500)
 
 
 class Application
