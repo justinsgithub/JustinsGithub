0b1e59eec068a6d2b034b39b4dbc8478e3793786
Justin Angeles <justinaawd@gmail.com>
2021-12-09 14:14:11 -0800

    Create classes.js

---
+++ JavaScript/Basics/classes.js
@@ -0,0 +1,30 @@
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get department() {
+    return this._department;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
+
+console.log(surgeonRomero.name)
+surgeonRomero.takeVacationDays(3)
+console.log(surgeonRomero.remainingVacationDays)
