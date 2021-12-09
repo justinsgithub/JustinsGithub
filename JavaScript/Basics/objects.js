b8c48d39c728da6ed198fb88b4d557bc7dbaeb7f
Justin Angeles <justinaawd@gmail.com>
2021-12-09 15:14:11 -0800

    Update objects.js

--- JavaScript/Basics/objects.js
+++ JavaScript/Basics/objects.js
@@ -1,6 +1,98 @@
+
+function robotFactory(model, mobile){
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+const robotFactory = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
+
+
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === "number"){
+      return `My current energy level is ${this._energyLevel}`
+    } else {
+      return "System malfunction: cannot retrieve energy level" 
+    }
+ },  
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num){
+    if(typeof this._numOfSensors === 'number' && num >= 0){
+      this._numOfSensors = num
+    } else {
+      console.log("Pass in a number that is greater than or equal to 0")
+    }
+  },
+  provideInfo(){
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}.`
+  },
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+
+const robotKeys = Object.keys(robot);
+console.log(robotKeys);
+
+const robotEntries = Object.entries(robot)
+
+console.log(robotEntries);
+
+const newRobot = Object.assign({
+  laserBlaster: true,
+  voiceRecognition: true
+}, robot)
+
+console.log(newRobot);
+robot.numOfSensors = 100
+console.log(robot.numOfSensors)
+console.log(robot.energyLevel)
+
+const { functionality } = robot
+
+functionality.beep()
+
+console.log(robot.provideInfo())
+
+
+
 let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
-
-
 
 let spaceship = {
   homePlanet: 'Earth',
