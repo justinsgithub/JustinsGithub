c616d94c7082b4db7eeb1bccf650d03de73209d4
Justin Angeles <justinaawd@gmail.com>
2021-12-11 12:51:51 -0800

    Update name-spaces.py

--- Python/DeeperPython/name-spaces.py
+++ Python/DeeperPython/name-spaces.py
@@ -1,1 +1,16 @@
+global_variable = 'global'
+ 
+def outer_function():
+  outer_value = "outer"
+ 
+  def inner_function():
+    inner_value = "inner"
 
+    def inner_nested_function():
+      nested_value = 'nested'
+    inner_nested_function()
+    # Add locals() below
+    print(locals())
+  inner_function()
+ 
+outer_function()
